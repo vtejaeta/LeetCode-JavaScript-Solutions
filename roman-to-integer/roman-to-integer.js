87becb7b963bd9ba4bc3d700658e54c949821a71
E. Viswa Teja <65386350+vtejaeta@users.noreply.github.com>
2021-10-05 14:17:06 +0530

    Time: 272 ms (14.15%), Space: 45.4 MB (40.87%) - LeetHub

--- roman-to-integer/roman-to-integer.js
+++ roman-to-integer/roman-to-integer.js
@@ -3,59 +3,24 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    let obj = {
-        "I" : 1,
-        "V" : 5,
-        "X" : 10,
-        "L" : 50,
-        "C" : 100,
-        "D" : 500,
-        "M" : 1000,
-    }, n = s.length, i = 0, final = 0;
-    
-    while(i < n){
-        let count;
-        if(s[i] == "I"){
-           if(s[i+1] == "V") {
-               count = 4;
-               i += 2;
-           }else if(s[i+1] == "X"){
-               count = 9;
-               i += 2;
-           }else {
-               count = obj[s[i]];
-               i++;
-           }
-        }
-        else if(s[i] == "X"){
-            if(s[i+1] == "L") {
-               count = 40;
-                i += 2;
-           }else if(s[i+1] == "C"){
-               count = 90;
-               i += 2;
-           }else {
-              count = obj[s[i]];
-               i++;
-           }
-       }
-        else if(s[i] == "C"){
-            if(s[i+1] == "D") {
-               count = 400;
-                i += 2;
-           }else if(s[i+1] == "M"){
-               count = 900;
-               i += 2;
-           }else{
-               count = obj[s[i]];
-               i++;
-           }
-        }else {
-            count = obj[s[i]];
-            i++;
-        }
-        final += count;
+    let ret = 0;
+    const dict = {
+      I: 1,
+      V: 5,
+      X: 10,
+      L: 50,
+      C: 100,
+      D: 500,
+      M: 1000,
+    };
+
+    for (let i = 0; i < s.length; i++) {
+      if (s[i + 1] && dict[s[i]] < dict[s[i + 1]]) {
+        ret -= dict[s[i]];
+      } else {
+        ret += dict[s[i]];
+      }
     }
-    
-    return final;
+
+    return ret;
 };
