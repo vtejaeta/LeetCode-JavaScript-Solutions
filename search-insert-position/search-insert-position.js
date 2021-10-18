22be1158880ea7c218df34179d0423a24d140d02
E. Viswa Teja <65386350+vtejaeta@users.noreply.github.com>
2021-10-18 14:33:39 +0530

    Time: 73 ms (64.51%), Space: 39.4 MB (78.85%) - LeetHub

---
+++ search-insert-position/search-insert-position.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let left = 0, right = nums.length - 1;
+    
+    while(left < right){
+        let middle = Math.floor((left+right) / 2);
+        
+        if(target == nums[middle]){
+            return middle;
+        }else if(target > nums[middle]){
+            left = middle + 1;
+        }else if(target < nums[middle]){
+            right = middle - 1;
+        }
+    }
+    
+    if(target == nums[left]){
+        return left;
+    }else if(target < nums[left]){
+        return left == 0 ? 0 : left;
+    }else{
+        return left + 1;
+    }
+};
