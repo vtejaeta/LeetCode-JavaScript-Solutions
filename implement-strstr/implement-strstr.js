ac9cbbfa81d21d7a2f38a7cce52474650ef575cd
E. Viswa Teja <65386350+vtejaeta@users.noreply.github.com>
2021-12-02 17:29:22 +0530

    Time: 4604 ms (9.34%), Space: 41.3 MB (6.13%) - LeetHub

--- implement-strstr/implement-strstr.js
+++ implement-strstr/implement-strstr.js
@@ -5,6 +5,21 @@
  */
 var strStr = function(haystack, needle) {
     if(needle == "") return 0;
+    if(needle.length > haystack.length) return -1;
     
-    return haystack.indexOf(needle);
+    for(let i = 0; i<haystack.length;i++){
+        if(haystack[i] == needle[0]){
+            for(let j = 0;j<needle.length;j++){
+                if(haystack[i+j] != needle[j]){
+                    break;
+                }
+                
+                if(j == needle.length-1){
+                    return i;
+                }
+            }
+        }
+    }
+    
+    return -1;
 };
