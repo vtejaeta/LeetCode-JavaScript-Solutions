b24606c86efb31f9032d88b2a4b7b6718b65d303
E. Viswa Teja <65386350+vtejaeta@users.noreply.github.com>
2021-12-31 10:07:07 +0530

    Time: 123 ms (8.75%), Space: 40.4 MB (92.27%) - LeetHub

--- binary-tree-level-order-traversal/binary-tree-level-order-traversal.js
+++ binary-tree-level-order-traversal/binary-tree-level-order-traversal.js
@@ -1,3 +1,37 @@
+// /**
+//  * Definition for a binary tree node.
+//  * function TreeNode(val, left, right) {
+//  *     this.val = (val===undefined ? 0 : val)
+//  *     this.left = (left===undefined ? null : left)
+//  *     this.right = (right===undefined ? null : right)
+//  * }
+//  */
+// /**
+//  * @param {TreeNode} root
+//  * @return {number[][]}
+//  */
+// var levelOrder = function(root) {
+//     if(root == null) return [];
+    
+//     let finalResult = [], queue = [root];
+    
+//     while(queue.length > 0){
+//         let size = queue.length;
+//         let nodesPerLevel = [];
+//         for(let i=0;i<size;i++){
+//             let node = queue.shift();
+//             nodesPerLevel.push(node.val);
+//             if(node.left != null) queue.push(node.left);
+//             if(node.right != null) queue.push(node.right);
+//         }
+//         finalResult.push(nodesPerLevel);
+//     }
+    
+//     return finalResult;
+    
+// };
+
+
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
@@ -11,22 +45,32 @@
  * @return {number[][]}
  */
 var levelOrder = function(root) {
-    if(root == null) return [];
+    let queue = [];
+    let result = [];
     
-    let finalResult = [], queue = [root];
+    if(root == null){
+        return result;
+    }
+    
+    queue.push(root);
     
     while(queue.length > 0){
         let size = queue.length;
         let nodesPerLevel = [];
         for(let i=0;i<size;i++){
-            let node = queue.shift();
-            nodesPerLevel.push(node.val);
-            if(node.left != null) queue.push(node.left);
-            if(node.right != null) queue.push(node.right);
+            let latestNode = queue.shift();
+            
+            if(latestNode.left){
+                queue.push(latestNode.left);
+            }
+            if(latestNode.right){
+                queue.push(latestNode.right);
+            }
+            nodesPerLevel.push(latestNode.val);
         }
-        finalResult.push(nodesPerLevel);
+        
+        result.push(nodesPerLevel);
     }
     
-    return finalResult;
-    
+    return result;
 };
