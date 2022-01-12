b8b5aac5312a01cbec98fd8b0491baaf56890587
E. Viswa Teja <65386350+vtejaeta@users.noreply.github.com>
2022-01-12 10:12:59 +0530

    Time: 222 ms (7.11%), Space: 51 MB (6.57%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.js
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.js
@@ -40,3 +40,30 @@ var insertIntoBST = function(root, val) {
     
     return root;
 };
+
+
+var insertIntoBST = function(root, val) {
+    if(root == null) return new TreeNode(val);
+    
+    let curr = root;
+    while(true){
+        if(val < curr.val){
+            if(curr.left != null){
+                curr = curr.left;
+            }else{
+                curr.left = new TreeNode(val);
+                break;
+            }
+        }
+        if(val > curr.val){
+            if(curr.right != null){
+                curr = curr.right;
+            }else{
+                curr.right = new TreeNode(val);
+                break;
+            }
+        }
+    }
+    
+    return root;
+};
