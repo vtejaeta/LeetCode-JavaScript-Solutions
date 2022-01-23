a04bfcf4c1f212ac915c47db4487a5d0888929d8
E. Viswa Teja <65386350+vtejaeta@users.noreply.github.com>
2022-01-23 14:19:09 +0530

    Time: 158 ms (5.67%), Space: 39.9 MB (65.64%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.js
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.js
@@ -18,15 +18,16 @@ var sumNumbers = function(root) {
 };
 
 let calcSum = (root, val = 0, ans) => {
-    if(root == null) return false;
+    if(root == null) return;
     
     val = val*10 + root.val;
-    let left = calcSum(root.left, val, ans);
-    let right = calcSum(root.right, val, ans);
     
-    if(left == false && right == false){
-        console.log(val);
+    if(root.left == null && root.right == null){
         ans[0] += val;
-        return true;
+        return;
     }
+    
+    calcSum(root.left, val, ans);
+    calcSum(root.right, val, ans);
+    
 }
